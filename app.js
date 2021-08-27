4e4d0f76cd58307f8d63be1e5a1108c362da829b
gustavohdev <g_a93@outlook.com>
2021-08-27 10:57:15 -0300

    first commit working ok

---
+++ app.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const redis = require('redis')
+const client = redis.createClient()
+const bcrypt = require('bcrypt')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, "views"))
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use(
+  session({
+    store: new RedisStore({ client: client }),
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 36000000, //10 hours, in milliseconds
+      httpOnly: false,
+      secure: false,
+    },
+    secret: 'bM80SARMxlq4fiWhulfNSeUFURWLTY8vyf',
+  })
+)
+
+//app.get('/', (req,res) => res.send('Hello World'))
+app.get('/', (req,res) => {
+  if(req.session.userid){
+    res.render("dashboard")
+  }else{
+    res.render('login')
+  }
+})
+
+
+
+app.post("/", (req, res) => {
+    const { username, password } = req.body
+
+    const saveSessionAndRenderDashboard = (userid) => {
+      req.session.userid = userid
+      req.session.save()
+      res.render("dashboard")
+    }
+
+    console.log(req.body, username, password)
+
+    if (!username || !password) {
+      res.render("error", {
+        message: "Please set both username and password",
+      })
+      return
+    }
+
+    client.hget('users', username, async (err, userid) => {
+      //console.log(userid)
+      const saltRounds = 10
+      //console.log(password, saltRounds)
+      const hash = await bcrypt.hash(password, saltRounds)
+      
+      if (!userid) {
+        //user does not exist, signup procedure
+        client.incr('userid', async (err, userid) => {
+            client.hset('users', username, userid)
+
+            client.hset(`user:${userid}`, 'hash', hash, 'username', username)
+
+            saveSessionAndRenderDashboard(userid)
+          
+        })
+      } else {
+        console.log('3')
+        //user exists, login procedure
+        client.hget(`user:${userid}`, 'hash', async (err, hash) => {
+          const result = await bcrypt.compare(password, hash)
+          if (result) {
+            //password ok
+            saveSessionAndRenderDashboard(userid)
+          } else {
+            //wrong password
+            res.render("error", {
+              message: "Incorrect password",
+            })
+            return
+          }
+        })
+      }
+    })
+    //res.end()
+  })
+
+app.listen(3000, () => console.log('Server Ready'))
+
+
